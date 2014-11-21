cee95aaba24469bb9bb43c42e317dd30076aa47f
ader148 <ader148@hotmail.com>
2014-11-21 15:29:38 -0500

    septimo cambio

--- stm3_project/src/main/Main.java
+++ stm3_project/src/main/Main.java
@@ -3,11 +3,14 @@ package main;
 public class Main {
 
 	public Main() {
+		System.out.println("septimo cambio en rama master");
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 		
 		System.out.println("proyecto original stm3");
 		System.out.println("primer cambio en rama master");
